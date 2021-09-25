119276ede16f842d91d323a1859b04cd4a64082e
NukeWolf <smart.alex.huang@gmail.com>
2021-09-25 16:52:49 -0400

    Added Class Sheet

--- config/classes.js
+++ config/classes.js
@@ -3,6 +3,7 @@ const Classes = [
     name: "CS 223",
     imgsrc:
       "https://upload.wikimedia.org/wikipedia/commons/1/18/C_Programming_Language.svg",
+    slug: "cs223",
   },
 ];
 export default Classes;
--- pages/index.js
+++ pages/index.js
@@ -1,47 +1,36 @@
 import { Layout, Menu, Breadcrumb, Col, Row, Card, Avatar } from "antd";
-import {
-  EditOutlined,
-  EllipsisOutlined,
-  SettingOutlined,
-} from "@ant-design/icons";
+import { ArrowRightOutlined } from "@ant-design/icons";
+import Classes from "../config/classes";
 
 const { Meta } = Card;
 const { Header, Content, Footer } = Layout;
 
 export default function Home() {
+  console.log(Classes);
   return (
     <Layout className="layout">
       <Header>
         <div className="logo" />
         <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
-          {new Array(15).fill(null).map((_, index) => {
-            const key = index + 1;
-            return <Menu.Item key={key}>{`nav ${key}`}</Menu.Item>;
-          })}
+          <Menu.Item>{`nav`}</Menu.Item>;
         </Menu>
       </Header>
       <Content style={{ padding: "0 50px" }}>
-        <Col>
-          <Row>
-            {" "}
-            <Card
-              style={{ width: 300 }}
-              cover={
-                <img
-                  alt="example"
-                  src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-                />
-              }
-              actions={[
-                <SettingOutlined key="setting" />,
-                <EditOutlined key="edit" />,
-                <EllipsisOutlined key="ellipsis" />,
-              ]}
-            >
-              <Meta title="Card title" description="This is the description" />
-            </Card>
-          </Row>
-        </Col>
+        <Row>
+          {Classes.map((classObj) => {
+            return (
+              <Col>
+                <Card
+                  style={{ width: 300 }}
+                  cover={<img alt="example" src={classObj.imgsrc} />}
+                  actions={[<ArrowRightOutlined key="Right" />]}
+                >
+                  <Meta title={classObj.name} />
+                </Card>
+              </Col>
+            );
+          })}
+        </Row>
       </Content>
       <Footer style={{ textAlign: "center" }}>
         Ant Design ©2018 Created by Ant UED
