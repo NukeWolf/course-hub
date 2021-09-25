dab9da3ad8c7fb1dc7c7907ae0f1ca04343bae33
NukeWolf <smart.alex.huang@gmail.com>
2021-09-25 18:00:08 -0400

    Index page done.

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,41 @@
-import { Layout, Menu, Breadcrumb, Col, Row, Card, Avatar } from "antd";
-import { ArrowRightOutlined } from "@ant-design/icons";
-import Classes from "../config/classes";
+import { useState, useEffect } from "react";
+import { Layout, Menu, Breadcrumb, Col, Row, Card, Button, Select } from "antd";
+import { ArrowRightOutlined, PlusCircleOutlined } from "@ant-design/icons";
+import Classes from "../config/classes.json";
+
+const courseMap = Classes.data.courses.map((course) => {
+  return {
+    courseCode: course.computed_listing_infos[0].course_code,
+    title: course.title,
+  };
+});
+console.log(courseMap);
 
 const { Meta } = Card;
 const { Header, Content, Footer } = Layout;
+const { Option } = Select;
 
 export default function Home() {
-  console.log(Classes);
+  const [myCourses, changeMyCourses] = useState(null);
+
+  if (typeof window !== "undefined" && myCourses == null) {
+    const rawCourses = window.localStorage.getItem("myCourses");
+    changeMyCourses(rawCourses);
+    if (myCourses == null) window.localStorage.setItem("myCourses", "");
+  }
+
+  const [courseAddInput, changeCourseAddInput] = useState("");
+  const onCourseChange = (input) => {
+    changeCourseAddInput(input);
+  };
+
+  const onCourseAdd = () => {
+    const courses = myCourses + "," + courseAddInput;
+    if (myCourses.includes(courseAddInput)) return;
+    changeMyCourses(courses);
+    window.localStorage.setItem("myCourses", courses);
+  };
+  console.log(courseAddInput);
   return (
     <Layout className="layout">
       <Header>
@@ -16,20 +45,60 @@ export default function Home() {
         </Menu>
       </Header>
       <Content style={{ padding: "0 50px" }}>
+        <Row style={{ margin: "20px" }} align="middle">
+          <Col>
+            <Select
+              showSearch
+              style={{ width: 200 }}
+              placeholder="Select a course"
+              optionFilterProp="children"
+              onChange={onCourseChange}
+              filterOption={(input, option) =>
+                option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+              }
+            >
+              {courseMap.map((course) => {
+                return (
+                  <Option value={course.courseCode}>{course.courseCode}</Option>
+                );
+              })}
+            </Select>
+          </Col>
+          <Col>
+            <Button
+              type="primary"
+              icon={<PlusCircleOutlined />}
+              size="large"
+              onClick={onCourseAdd}
+            >
+              Add new course
+            </Button>
+          </Col>
+        </Row>
         <Row>
-          {Classes.map((classObj) => {
-            return (
-              <Col>
-                <Card
-                  style={{ width: 300 }}
-                  cover={<img alt="example" src={classObj.imgsrc} />}
-                  actions={[<ArrowRightOutlined key="Right" />]}
-                >
-                  <Meta title={classObj.name} />
-                </Card>
-              </Col>
-            );
-          })}
+          {myCourses
+            ?.split(",")
+            .filter((st) => st !== "")
+            .map((classString) => {
+              return (
+                <Col>
+                  <Card
+                    style={{ width: 300 }}
+                    cover={<img alt="example" />}
+                    actions={[<ArrowRightOutlined key="Right" />]}
+                  >
+                    <Meta
+                      title={classString}
+                      description={
+                        courseMap.find(
+                          (course) => course.courseCode == classString
+                        ).title
+                      }
+                    />
+                  </Card>
+                </Col>
+              );
+            })}
         </Row>
       </Content>
       <Footer style={{ textAlign: "center" }}>
